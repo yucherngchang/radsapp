4873264ce159c0e9e9dc2677b9d1b427f0083cf7
channing <yu-cherng.chang@som.umaryland.edu>
2022-04-07 12:56:51 -0400

    before magnify

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # radsapp
 Test your knowledge of CXR findings
+This repo contains code for self-quizzing of radiographic findings derived from NIH database (https://nihcc.app.box.com/v/ChestXray-NIHCC),
+estimated to be ~90% accurate.
+To function properly, images need to be downloaded locally and then placed in images folder and corresponding groundtruth pasted into getcsv() function in index.js. 
--- index.js
+++ index.js
@@ -1,113 +1,171 @@
-function loadImage(){
-	[].forEach.call(document.querySelectorAll('.titles'), function (el) {
-  el.style.visibility = 'hidden';
-});
+function loadImage() {
+    [].forEach.call(document.querySelectorAll('.titles'), function (el) {
+        el.style.visibility = 'hidden';
+    });
 	
-	document.getElementById("tutorialblock").textContent = '';
-	[].forEach.call(document.querySelectorAll('.tutorialBlock'), function (el) {
-  el.style.visibility = 'hidden';
-});
+	[].forEach.call(document.querySelectorAll('#check'), function (el) {
+        el.style.visibility = 'visible';
+    });
+    [].forEach.call(document.querySelectorAll('#filterButton'), function (el) {
+        el.style.visibility = 'hidden';
+    });
 
-	var data = Papa.parse(getcsv());
+    document.getElementById("tutorialblock").textContent = '';
+    [].forEach.call(document.querySelectorAll('.tutorialBlock'), function (el) {
+        el.style.visibility = 'hidden';
+    });
+
+    var data = Papa.parse(getcsv());
+
+    var filtered = document.getElementById("filter").innerHTML.split("|");
+
+    [].forEach.call(document.querySelectorAll('input[type=checkbox]'), function (el) {
+        el.checked = false;
+        if (!filtered.includes(el.value)) {
+            el.disabled = true;
+        }
+    });
 	
-	var ind = Math.floor(Math.random() * (data.data.length-1));
-	document.getElementById("image").src="./images/"+data.data[ind][0];
-	document.getElementById("secret").innerHTML=data.data[ind][1];
-	[].forEach.call(document.querySelectorAll('.answer'), function (el) {
-  el.style.visibility = 'visible';
-});
-	[].forEach.call(document.querySelectorAll('input[type=checkbox]'), function (el) {
-  el.checked = false;
-});
-	document.getElementById("reveal").innerHTML="";
+    var flag = 0;
+    while (flag == 0) {
+        var ind = Math.floor(Math.random() * (data.data.length - 1));
+		var answer=data.data[ind][1].split("|");
+		if (answer.every(val => filtered.includes(val))){
+			flag=1;
+			document.getElementById("image").src = "./images/" + data.data[ind][0];
+			document.getElementById("secret").innerHTML = data.data[ind][1];
+			[].forEach.call(document.querySelectorAll('.answer'), function (el) {
+				el.style.visibility = 'visible';
+			});
+		}
+    }
+
+    document.getElementById("reveal").innerHTML = "";
+    document.getElementById("check").disabled = false;
+	magnify("image", 3);
 }
 
-function loadTutorial(){
-	[].forEach.call(document.querySelectorAll('.titles'), function (el) {
-  el.style.visibility = 'hidden';
-});
-	[].forEach.call(document.querySelectorAll('.tutorialBlock'), function (el) {
-  el.style.visibility = 'visible';
-});
-[].forEach.call(document.querySelectorAll('.answer'), function (el) {
-  el.style.visibility = 'hidden';
-});
-	var text=["blunting of costophrenic recesses", "opacity >3 cm diameter","retrocardiac stomach, retrocardiac air-fluid levels",
-	"heart >50% of thoracic cavity","opacity <3 cm diameter",
-	"increased opacification with displacement of lung fissures",
-"flattening of the bilateral hemidiaphragms, increased lung volumes, increased AP diameter",
-"multiple opacities","lucency w/o vascular markings, mediastinal shift"];
-	document.getElementById("tutorialblock").textContent = '';
-	
-	var checkedBoxes = document.querySelectorAll('input[type=checkbox]');
-	
-	var choices=[];
-	for (let i = 0; i < checkedBoxes.length; i++) {
-  choices[i]=checkedBoxes[i].value;
-}
-console.log(choices);
-	
-	for (let i = 1; i < choices.length; i++){ 
-	var newA = document.createElement("button");
-	var linkText = document.createTextNode(choices[i]);
-	newA.appendChild(linkText);
-	newA.id=choices[i];
-	newA.onclick = function() { document.getElementById("image").src="./tutimages/"+choices[i]+".jpg"; };
-	var newP = document.createElement("p");
-	newP.innerHTML=text[i-1];
-	document.getElementById("tutorialblock").insertBefore(newP, document.getElementById("tutorialblock").firstChild);
-	document.getElementById("tutorialblock").insertBefore(newA, document.getElementById("tutorialblock").firstChild);
-	}
-	var newP = document.createElement("p");
-	newP.innerHTML="Images courtesy of MedPix";
-	document.getElementById("tutorialblock").insertBefore(newP, document.getElementById("tutorialblock").firstChild);
+function filter() {
+    [].forEach.call(document.querySelectorAll('.titles'), function (el) {
+        el.style.visibility = 'hidden';
+    });
+    [].forEach.call(document.querySelectorAll('.answer'), function (el) {
+        el.style.visibility = 'visible';
+    });
+    [].forEach.call(document.querySelectorAll('input[type=checkbox]'), function (el) {
+        el.checked = true;
+        el.disabled = false;
+    });
+    [].forEach.call(document.querySelectorAll('#check'), function (el) {
+        el.style.visibility = 'hidden';
+    });
+    [].forEach.call(document.querySelectorAll('#filterButton'), function (el) {
+        el.style.visibility = 'visible';
+    });
 
-
-	
 }
 
+function filterCases() {
+    var checkedBoxes = document.querySelectorAll('input[type=checkbox]:checked');
 
-function check(){
-	var str=document.getElementById("secret").innerHTML;
-	var answers = str.split("|");
-	
-	var checkedBoxes = document.querySelectorAll('input[type=checkbox]:checked');
-	
-	var checked=[];
-	for (let i = 0; i < checkedBoxes.length; i++) {
-  checked[i]=checkedBoxes[i].value;
+    var str = "";
+    for (let i = 0; i < checkedBoxes.length; i++) {
+        str = str + checkedBoxes[i].value + "|";
+    }
+    document.getElementById("filter").innerHTML = str.slice(0, -1);
 }
-	if (arraysEqual(checked, answers) == true){
-		document.getElementById("reveal").innerHTML="correct!";
-	}else{
-		var str2="";
-		for (let i = 0; i < answers.length; i++) {
-  str2=str2+" "+answers[i];
+
+function loadTutorial() {
+    [].forEach.call(document.querySelectorAll('.titles'), function (el) {
+        el.style.visibility = 'hidden';
+    });
+    [].forEach.call(document.querySelectorAll('.tutorialBlock'), function (el) {
+        el.style.visibility = 'visible';
+    });
+    [].forEach.call(document.querySelectorAll('.answer'), function (el) {
+        el.style.visibility = 'hidden';
+    });
+    var text = ["blunting of costophrenic recesses", "opacity >3 cm diameter", "retrocardiac stomach, retrocardiac air-fluid levels",
+        "heart >50% of thoracic cavity", "opacity <3 cm diameter",
+        "increased opacification with displacement of lung fissures",
+        "flattening of the bilateral hemidiaphragms, increased lung volumes, increased AP diameter",
+        "multiple opacities", "lucency w/o vascular markings, mediastinal shift"];
+    document.getElementById("tutorialblock").textContent = '';
+
+    var checkedBoxes = document.querySelectorAll('input[type=checkbox]');
+
+    var choices = [];
+    for (let i = 0; i < checkedBoxes.length; i++) {
+        choices[i] = checkedBoxes[i].value;
+    }
+    console.log(choices);
+
+    for (let i = 1; i < choices.length; i++) {
+        var newA = document.createElement("button");
+        var linkText = document.createTextNode(choices[i]);
+        newA.appendChild(linkText);
+        newA.id = choices[i];
+        newA.onclick = function () {
+            document.getElementById("image").src = "./tutimages/" + choices[i] + ".jpg";
+        };
+        var newP = document.createElement("p");
+        newP.innerHTML = text[i - 1];
+        document.getElementById("tutorialblock").insertBefore(newP, document.getElementById("tutorialblock").firstChild);
+        document.getElementById("tutorialblock").insertBefore(newA, document.getElementById("tutorialblock").firstChild);
+    }
+
 }
-		
-		document.getElementById("reveal").innerHTML="incorrect, answers are "+str2;
-	}
+
+
+
+function check() {
+    var str = document.getElementById("secret").innerHTML;
+    var answers = str.split("|");
+
+    var checkedBoxes = document.querySelectorAll('input[type=checkbox]:checked');
+
+    var checked = [];
+    for (let i = 0; i < checkedBoxes.length; i++) {
+        checked[i] = checkedBoxes[i].value;
+    }
+    var score = document.getElementById("score").innerHTML;
+    var scoreSplit = score.split("/");
+    if (arraysEqual(checked, answers) == true) {
+        document.getElementById("score").innerHTML = (Number(scoreSplit[0]) + 1).toString() + "/" + (Number(scoreSplit[1]) + 1).toString();
+        document.getElementById("reveal").innerHTML = "Correct, your score is " + document.getElementById("score").innerHTML;
+    } else {
+        var str2 = "";
+        for (let i = 0; i < answers.length; i++) {
+            str2 = str2 + " " + answers[i];
+        }
+        document.getElementById("score").innerHTML = Number(scoreSplit[0]).toString() + "/" + (Number(scoreSplit[1]) + 1).toString();
+        document.getElementById("reveal").innerHTML = "Incorrect, answers are " + str2 + ". Your score is " + document.getElementById("score").innerHTML;
+        document.getElementById("check").disabled = true;
+    }
 }
 
 function arraysEqual(a, b) {
-  if (a === b) return true;
-  if (a == null || b == null) return false;
-  if (a.length !== b.length) return false;
+    if (a === b)
+        return true;
+    if (a == null || b == null)
+        return false;
+    if (a.length !== b.length)
+        return false;
 
-  // If you don't care about the order of the elements inside
-  // the array, you should sort both arrays here.
-  // Please note that calling sort on an array will modify that array.
-  // you might want to clone your array first.
+    // If you don't care about the order of the elements inside
+    // the array, you should sort both arrays here.
+    // Please note that calling sort on an array will modify that array.
+    // you might want to clone your array first.
 
-  for (var i = 0; i < a.length; ++i) {
-    if (a[i] !== b[i]) return false;
-  }
-  return true;
+    for (var i = 0; i < a.length; ++i) {
+        if (a[i] !== b[i])
+            return false;
+    }
+    return true;
 }
 
-
-function getcsv(){
-	return `Image Index,Finding Labels,Follow-up #,Patient ID,Patient Age,Patient Gender,View Position,OriginalImage[Width,Height],OriginalImagePixelSpacing[x,y]
+function getcsv() {
+    return `Image Index,Finding Labels,Follow-up #,Patient ID,Patient Age,Patient Gender,View Position,OriginalImage[Width,Height],OriginalImagePixelSpacing[x,y]
 00000001_000.png,Cardiomegaly,0,1,57,M,PA,2682,2749,0.14300000000000002,0.14300000000000002
 00000001_001.png,Cardiomegaly|Emphysema,1,1,58,M,PA,2894,2729,0.14300000000000002,0.14300000000000002
 00000001_002.png,Cardiomegaly|Effusion,2,1,58,M,PA,2500,2048,0.168,0.168
