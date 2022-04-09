f70a91878d958aec5d5c3e3131ab71332fa0f46c
channing <yu-cherng.chang@som.umaryland.edu>
2022-04-09 13:27:35 -0400

    added quiz and magnifying glass

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ function loadImage() {
 		var answer=data.data[ind][1].split("|");
 		if (answer.every(val => filtered.includes(val))){
 			flag=1;
+			document.getElementById("image").onload = function() { magnify("image", 3); }
 			document.getElementById("image").src = "./images/" + data.data[ind][0];
 			document.getElementById("secret").innerHTML = data.data[ind][1];
 			[].forEach.call(document.querySelectorAll('.answer'), function (el) {
@@ -42,8 +43,77 @@ function loadImage() {
 
     document.getElementById("reveal").innerHTML = "";
     document.getElementById("check").disabled = false;
-	magnify("image", 3);
+	
 }
+
+function magnify(imgID, zoom) {
+  if (document.getElementsByClassName("img-magnifier-glass").length>0){
+	  
+	var r=document.getElementsByClassName("img-magnifier-glass");
+	console.log(r);
+	r[0].parentNode.removeChild(r[0]);
+  }
+  var img, glass, w, h, bw;
+  img = document.getElementById(imgID);
+
+  /* Create magnifier glass: */
+  glass = document.createElement("DIV");
+  glass.setAttribute("class", "img-magnifier-glass");
+
+  /* Insert magnifier glass: */
+  img.parentElement.append(glass);
+
+  /* Set background properties for the magnifier glass: */
+  glass.style.backgroundImage  = "url('" + img.src + "')";
+  glass.style.backgroundRepeat = "no-repeat";
+  glass.style.backgroundSize = (img.width * zoom) + "px " + (img.height * zoom) + "px";
+
+  bw = 3;
+  w = glass.offsetWidth / 2;
+  h = glass.offsetHeight / 2;
+
+  /* Execute a function when someone moves the magnifier glass over the image: */
+  glass.addEventListener("mousemove", moveMagnifier);
+  img.addEventListener("mousemove", moveMagnifier);
+
+  /*and also for touch screens:*/
+  glass.addEventListener("touchmove", moveMagnifier);
+  img.addEventListener("touchmove", moveMagnifier);
+  function moveMagnifier(e) {
+    var pos, x, y;
+    /* Prevent any other actions that may occur when moving over the image */
+    e.preventDefault();
+    /* Get the cursor's x and y positions: */
+    pos = getCursorPos(e);
+    x = pos.x;
+    y = pos.y;
+    /* Prevent the magnifier glass from being positioned outside the image: */
+    if (x > img.width - (w / zoom)) {x = img.width - (w / zoom);}
+    if (x < w / zoom) {x = w / zoom;}
+    if (y > img.height - (h / zoom)) {y = img.height - (h / zoom);}
+    if (y < h / zoom) {y = h / zoom;}
+    /* Set the position of the magnifier glass: */
+    glass.style.left = (x - w) + "px";
+    glass.style.top = (y - h) + "px";
+    /* Display what the magnifier glass "sees": */
+    glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
+  }
+
+  function getCursorPos(e) {
+    var a, x = 0, y = 0;
+    e = e || window.event;
+    /* Get the x and y positions of the image: */
+    a = img.getBoundingClientRect();
+    /* Calculate the cursor's x and y coordinates, relative to the image: */
+    x = e.pageX - a.left;
+    y = e.pageY - a.top;
+    /* Consider any page scrolling: */
+    x = x - window.pageXOffset;
+    y = y - window.pageYOffset;
+    return {x : x, y : y};
+  }
+}
+
 
 function filter() {
     [].forEach.call(document.querySelectorAll('.titles'), function (el) {
@@ -105,14 +175,38 @@ function loadTutorial() {
         var linkText = document.createTextNode(choices[i]);
         newA.appendChild(linkText);
         newA.id = choices[i];
+		newA.className="accordion";
         newA.onclick = function () {
             document.getElementById("image").src = "./tutimages/" + choices[i] + ".jpg";
         };
+		var newD = document.createElement("div");
+		newD.className="panel";
         var newP = document.createElement("p");
         newP.innerHTML = text[i - 1];
-        document.getElementById("tutorialblock").insertBefore(newP, document.getElementById("tutorialblock").firstChild);
+		newD.appendChild(newP);
+		document.getElementById("tutorialblock").insertBefore(newD, document.getElementById("tutorialblock").firstChild);
         document.getElementById("tutorialblock").insertBefore(newA, document.getElementById("tutorialblock").firstChild);
     }
+	
+	var acc = document.getElementsByClassName("accordion");
+	var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
 
 }
 
